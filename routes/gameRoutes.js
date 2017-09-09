35c6732a7b74f5dc4672a9d617db1cad01a0e72e
ctvanzandt42 <ctvanzandt42@gmail.com>
2017-09-09 16:49:27 -0500

    ready to turn in

--- routes/gameRoutes.js
+++ routes/gameRoutes.js
@@ -1,4 +1,72 @@
 const express = require('express');
+const fs = require('fs');
 const gameRoutes = express.Router();
+const words = fs.readFileSync("/usr/share/dict/words", "utf-8").toUpperCase().split("\n");
+
+function getRandomInt(min, max) {
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+gameRoutes.get("/newgame", (req, res) => {
+    const randomWord = words[getRandomInt(0, words.length - 1)];
+    req.session.word = randomWord;
+    let displayArray = [];
+    for (let i = 0; i < randomWord.length; i++) {
+        displayArray.push("_ ");
+    }
+    req.session.display = displayArray;
+    req.session.wrongGuesses = [];
+    req.session.correctGuesses = [];
+    req.session.turns = 8;
+    req.session.playing = true;
+    req.session.msg = `Guess a letter!`;
+    res.redirect("/");
+});
+
+gameRoutes.post("/guess", (req, res) => {
+    let guessLetter = req.body.letter.toUpperCase();
+    let word = req.session.word;
+    console.log(word);
+
+    if ((req.session.playing = false)) {
+        return res.redirect("/");
+    }
+
+    if (
+        req.session.wrongGuesses.indexOf(guessLetter) > -1 ||
+        req.session.correctGuesses.indexOf(guessLetter) > -1
+    ) {
+        req.session.msg = `Already guessed!`;
+        return res.redirect("/");
+    } else if (word.indexOf(guessLetter) < 0) {
+        req.session.turns--;
+        if (req.session.turns < 1) {
+            req.session.playing = false;
+            req.session.msg =
+                `Sorry! The word was ${req.session.word}! You suck! Click the button above to play again!`;
+            return res.redirect("/");
+        } else {
+            req.session.msg = `Sorry, guess again! ${req.session.turns} turns left!`;
+            req.session.wrongGuesses.push(guessLetter);
+            return res.redirect("/");
+        }
+    } else
+        for (let i = 0; i < word.length; i++) {
+            if (word.charAt(i).toUpperCase() === guessLetter) {
+                req.session.msg =
+                    `Correct! ${req.session.turns} turns left!`;
+                req.session.display[i] = guessLetter;
+                if (req.session.correctGuesses.indexOf(guessLetter) < 0) {
+                    req.session.correctGuesses.push(guessLetter);
+                }
+                if (req.session.display.join("") === req.session.word) {
+                    req.session.playing = false;
+                    req.session.msg = `You win! Click the button above to play again!`;
+                }
+            }
+        }
+
+    return res.redirect("/");
+});
 
 module.exports = gameRoutes;
--- server.js
+++ server.js
@@ -1,29 +1,32 @@
-const express = require('express');
-const expressValidator = require('express-validator');
-const mustacheExpress = require('mustache-express');
-const logger = require('morgan');
-const bodyParser = require('body-parser');
-const session = require('express-session');
-const fs = require('fs');
-const sessionConfig = require('./sessionConfig');
+const express = require("express");
+const expressValidator = require("express-validator");
+const bodyParser = require("body-parser");
+const session = require("express-session");
+const logger = require("morgan");
+const mustacheExpress = require("mustache-express");
+const path = require("path");
+const fs = require("fs");
 const words = fs.readFileSync("/usr/share/dict/words", "utf-8").toUpperCase().split("\n");
+const sessionConfig = require("./sessionConfig");
 const indexRoutes = require('./routes/indexRoutes');
 const gameRoutes = require('./routes/gameRoutes');
 
 const app = express();
-const path = require('path');
-const port = process.env.PORT || 4000;
+const port = process.env.PORT || 8000;
+
+app.engine("mustache", mustacheExpress());
+app.set("views", "./views");
+app.set("view engine", "mustache");
 
 app.listen(port, () => {
-    console.log(`Server running on port ${port}!`);
+    console.log(`Currently running on port ${port}.`);
 });
-app.engine('mustache', mustacheExpress);
-app.set('views', './views');
-app.set('view engine', 'mustache');
-app.use(express.static(path.join(__dirname, './public')));
+
+app.use(express.static(path.join(__dirname, "public")));
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(session(sessionConfig));
-app.use(expressValidator);
+app.use(expressValidator());
+app.use(logger('dev'));
 
 app.use('/', indexRoutes);
 app.use('/game', gameRoutes);
